73b54248871c840ecc873f86741a69d63234835a
nurassyl <kairzhannurasyl7@gmail.com>
2025-04-02 20:55:46 +0500

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from webapp.app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Welcome to Flask CI/CD project"
